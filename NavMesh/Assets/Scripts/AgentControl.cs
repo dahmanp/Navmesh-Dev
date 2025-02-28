2071e9f761e45941eb4181650825df78320565e0
dahmanp <dahmanp@my.erau.edu>
2025-02-27 19:26:25 -0700

    Add Build & Project

---
+++ NavMesh/Assets/Scripts/AgentControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentControl : MonoBehaviour
+{
+    public Transform home;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(home.position);
+    }
+}
